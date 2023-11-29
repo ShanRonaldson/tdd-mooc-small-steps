d23d933edc6ae299d7a9861f2a7eb756c57bd43b
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2023-11-29 16:49:40 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -27,7 +27,7 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      return new Date(dateString);
+      return Temporal.PlainDate.from(dateString)
     }
   }
   function parseTemporalDate(dateString){if(dateString){return Temporal.PlainDate.from(dateString)}}
