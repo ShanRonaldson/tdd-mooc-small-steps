972f31e0bf6960502016947646d5f71149071b41
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2023-11-29 16:45:14 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -88,7 +88,7 @@ function createApp(database) {
     for (let row of holidays) {
       let parsedHoliday = Temporal.PlainDate.from(row.holiday);
       if (
-        (day || temporalDate) &&
+        (date || temporalDate) &&
         (day.getFullYear() || temporalDate.year ) === parsedHoliday.year &&
         day.getMonth() === (parsedHoliday.month - 1) && //TODO fix me
         (day.getDate() || temporalDate.day) === parsedHoliday.day
