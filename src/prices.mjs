022989c027183883d41a58a664f95a90a76c4282
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2023-11-29 16:50:47 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -85,7 +85,6 @@ function createApp(database) {
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let parsedHoliday = Temporal.PlainDate.from(row.holiday);
       let holiday = Temporal.PlainDate.from(row.holiday);
       if (
         date &&
