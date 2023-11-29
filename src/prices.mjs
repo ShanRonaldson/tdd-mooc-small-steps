b410ef1478c924f113e5df9b4250c56f46fd43b1
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2023-11-29 16:50:43 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -91,7 +91,7 @@ function createApp(database) {
         date &&
         date.year === holiday.year &&
         date.month === holiday.month &&
-        date.day === parsedHoliday.day
+        date.day === holiday.day
       ) {
         return true;
       }
